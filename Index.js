aead289f352e013025c7071ce5a0bc09cb0069ee
lizbet08 <45060454+lizbet08@users.noreply.github.com>
2020-06-26 11:44:57 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,18 +1,19 @@
-var num1, num2;
-num1 = window.prompt("Input the First integer", "0");
-num2 = window.prompt("Input the second integer", "0");
+Const num1, num2;
+Const num1 = window.prompt("Input the First integer", "0");
+Const num2 = window.prompt("Input the second integer", "0");
                                                  
-if(parseInt(num1, 25) > parseInt(num2, 20)) 
+if(num1, 25) > (num2, 30) 
   { 
-  console.log("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");
+  document.write(`The larger of ${num1} and ${num2} is ${num1}`);
   }   
 else
-  if(parseInt(num2, 30) > parseInt(num1, 20)) 
+  if(num2, 30) > (num1, 20)
   {
-  console.log("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");
+  document.wtite(`The larger of ${num1} and ${num2} is ${num2}`);
+ 
   }                  
 else
   {
-   console.log("The values "+ num1+ " and "+num2+ " are equal.");
+   document.write(`The values of ${num1} and ${num2} are equal.`);
   }
   
